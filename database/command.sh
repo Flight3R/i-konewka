720be70697eceaf4360c1a934e352f84a0a42edd
Flight3R <michaal747@gmail.com>
2023-12-18 18:15:20 +0100

    Readme added

--- database/command.sh
+++ database/command.sh
@@ -6,11 +6,3 @@ PROD:
 DEV:
     docker build -t ikonewka_mysql_dev_image .
     docker run -d -p 3306:3306 --name ikonewka_mysql_dev_container ikonewka_mysql_dev_image
-
-PROD:
-    docker build -t ikonewka_backend_image -f Dockerfile_prod .
-    docker run -d -p 60001:8000 --name ikonewka_backend_container --network ikonewka_network ikonewka_backend_image
-DEV:
-    docker build -t ikonewka_backend_dev_image -f Dockerfile_dev .
-    docker run -d -p 60001:5000 --name ikonewka_backend_dev_container --network ikonewka_network ikonewka_backend_dev_image
-
